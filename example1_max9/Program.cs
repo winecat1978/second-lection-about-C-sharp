51323ad57db4ffb1f13dc262d9762d2df9ffd37f
Katherine_20 <mob1911494@gmail.com>
2022-05-04 23:17:38 +0300

    i watched 2nd lection

---
+++ example1_max9/Program.cs
@@ -0,0 +1,29 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{ int result = arg1;
+if (arg2 > result) result = arg2;
+if (arg3 > result) result = arg3;
+return result;
+}
+
+int a1 = 12;
+int b1 = 3400;
+int c1 = 7;
+int a2 = 4309;
+int b2 = 98;
+int c2 = 721;
+int a3 = 18;
+int b3 = 23123;
+int c3 = 39873;
+
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max (max1, max2, max3);
+ 
+
+int max = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
+Console.WriteLine (max);
---
+++ example2_find_element/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {111,224,18,114,51,76,27,18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1
+    index++;
+}
